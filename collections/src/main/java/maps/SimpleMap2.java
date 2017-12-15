3ab6bcc90e310939ec80f1f5f9f986dfebcb1ff1
permedont <itapodoz1@mail.ru>
2017-12-15 15:30:31 +0300

    commit8

--- collections/src/main/java/maps/SimpleMap2.java
+++ collections/src/main/java/maps/SimpleMap2.java
@@ -2,6 +2,6 @@ package maps;
 
 public class SimpleMap2 {
     public static void main(final String[] vars) {
-        System.out.println("1");//TODO: 1
+        System.out.println("1");//TODO: 2
     }
 }
