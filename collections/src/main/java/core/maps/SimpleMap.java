59d56401ab5205cf8ac47a83e6763959c516c9ed
permedont <itapodoz1@mail.ru>
2017-12-16 15:30:58 +0300

    commit

--- collections/src/main/java/core/maps/SimpleMap.java
+++ collections/src/main/java/core/maps/SimpleMap.java
@@ -1,15 +1,32 @@
 package core.maps;
 
-import java.util.Map;
-import java.util.TreeMap;
+import com.sun.istack.internal.NotNull;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+import java.util.stream.Stream;
 
 
 public class SimpleMap {
-    public static void main(final String[] vars) {
-        final Map<String, String> m1 = new TreeMap<String, String>();
-        m1.put(null, "test");
-        m1.put(null, "test");
-        m1.put(null, "null");
-        System.out.println(m1.get(null));
+
+    private static Map getMap(final Class<? extends Map> clazz) {
+        try {
+            return clazz.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    private static void checkMap(@NotNull final Map<String,String> m)
+    {
+        m.put("1", "test");
+        m.put("1", "test");
+        m.put("1", "null");
+    }
+
+    public static void main(final String[] vars) throws Exception {
+        Stream.of(HashMap.class, LinkedHashMap.class, TreeMap.class).forEach((m) -> checkMap(getMap(m)));
     }
 }
